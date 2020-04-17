159cc84119a618dc8591a97198025cc08c2b8d0d
Daniel Wilde <wildedh@gmail.com>
2020-04-16 20:12:31 -0400

    New file, testing

--- Oil_gas.py
+++ Oil_gas.py
@@ -1,6 +1,8 @@
-import ocrmypdf , tesseract
+import ocrmypdf
+
+
 def ocr(file_path, save_path):
     ocrmypdf.ocr(file_path, save_path, rotate_pages=True,
     remove_background=True,language="en", deskew=True, force_ocr=True)
 
-
+ocr()
