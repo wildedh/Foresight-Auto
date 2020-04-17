2d4990100a07f19fa02a9358587afb9cc94705ee
Daniel Wilde <wildedh@gmail.com>
2020-04-16 20:26:43 -0400

    New file, testing

--- Oil_gas.py
+++ Oil_gas.py
@@ -1,8 +1,13 @@
 import ocrmypdf
 
-
+"""
 def ocr(file_path, save_path):
     ocrmypdf.ocr(file_path, save_path, rotate_pages=True,
     remove_background=True,language="en", deskew=True, force_ocr=True)
+"""
 
-ocr()
+def ocr(input_pdf_or_image, output_pdf):
+    ocrmypdf.ocr(input_pdf_or_image, output_pdf, rotate_pages=True,
+    remove_background=True, language="en", deskew=True, force_ocr=True)
+
+ocr("/Users/danielwilde/Desktop/BP1982_UK.pdf", "‎/Users/danielwilde/Desktop/BP1982_UK_searchable.pdf")
